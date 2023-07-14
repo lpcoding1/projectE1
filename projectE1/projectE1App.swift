a8ae3ad74737bd2dfcc7ab4e8bc2531befef3c9d
Lena <lenapi3207@gmail.com>
2023-07-14 10:46:47 -0400

    Initial Commit

---
+++ projectE1/projectE1App.swift
@@ -0,0 +1,17 @@
+//
+//  projectE1App.swift
+//  projectE1
+//
+//  Created by Lena Pianfetti on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct projectE1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        } 
+    }
+}
